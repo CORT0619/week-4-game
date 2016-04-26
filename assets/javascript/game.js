24bd2417a660894e77be0180843cba25fd64a3ab
Crystal Williams <cortney0215@yahoo.com>
2016-04-25 22:39:24 -0400

    change setup of characters array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,31 @@
 	
-	var characters = [{name: "Sponge Bob", healthPoints: 120, attackPower: 8, counterAttackPower: 10, src: "assets/images/spongeBob.jpg", index: 0}, {name: "Darth Vader", healthPoints: 100, attackPower: 10, counterAttackPower: 6, src: "assets/images/darth.jpg"}, {name: "Tigger", healthPoints: 150, attackPower: 12, counterAttackPower: 5, src: "assets/images/tigger.gif"}, {name: "Tweety", healthPoints: 180, attackPower: 9, counterAttackPower: 9, src: "assets/images/tweety.gif"}];
+	/*var characters = [{name: "Sponge Bob", healthPoints: 120, attackPower: 8, counterAttackPower: 10, src: "assets/images/spongeBob.jpg", index: 0}, {name: "Darth Vader", healthPoints: 100, attackPower: 10, counterAttackPower: 6, src: "assets/images/darth.jpg"}, {name: "Tigger", healthPoints: 150, attackPower: 12, counterAttackPower: 5, src: "assets/images/tigger.gif"}, {name: "Tweety", healthPoints: 180, attackPower: 9, counterAttackPower: 9, src: "assets/images/tweety.gif"}];*/
 	
+/*var myCharLoc = $('#myCharacter p.hps').data('ind');
+var myBaseAttack = characters[myCharLoc].healthPoints;
+
+var compCharLoc = $('#battleField p.hps').data('ind');
+var compBaseAttack = characters[compCharLoc].healthPoints;*/
+
 
 	var game = {
 		wins: 0,
-		losses: 0,
 		personHPs: 0,
 		computerHPs: 0,
+
+		/*myCharLoc: $('#myCharacter p.hps').data('ind'),
+		myBaseAttack: this.characters[myCharLoc].healthPoints,
+
+		compCharLoc: $('#battleField p.hps').data('ind'),
+		compBaseAttack: this.characters[compCharLoc].healthPoints,*/
+
+
+		characters: [{name: "Sponge Bob", healthPoints: 120, attackPower: 8, counterAttackPower: 10, src: "assets/images/spongeBob.jpg", index: 0}, {name: "Darth Vader", healthPoints: 100, attackPower: 10, counterAttackPower: 6, src: "assets/images/darth.jpg"}, {name: "Tigger", healthPoints: 150, attackPower: 12, counterAttackPower: 5, src: "assets/images/tigger.gif"}, {name: "Tweety", healthPoints: 180, attackPower: 9, counterAttackPower: 9, src: "assets/images/tweety.gif"}], 
 		
 
 		attack: function(){
 			
-			var myCharLoc = $('#myCharacter p.hps').data('ind');
-			var compCharLoc = $('#battleField p.hps').data('ind');
-
-
-			if(characters[myCharLoc].healthPoints <= 0){
+			if(this.characters[myCharLoc].healthPoints <= 0){
 
 				$('p#gameStats').html("You've been defeated..GAME OVER!!");
 
@@ -24,22 +34,30 @@
 				newBut.attr('id', 'startOver');
 				$('p#gameStats').append(newBut);
 
-			} else if(characters[compCharLoc].healthPoints <= 0){
+			} else if(this.characters[compCharLoc].healthPoints <= 0){
 
-				$('p#gameStats').html("You have defeated " + characters[compCharLoc].name + ", you can choose to fight another enemy.");
+				$('p#gameStats').html("You have defeated " + this.characters[compCharLoc].name + ", you can choose to fight another enemy.");
+
+				$('#battleField').children("div.imgCharacters").remove();
 
 			} else {
 
 				//update player 1's health
-				characters[myCharLoc].healthPoints -= characters[compCharLoc].counterAttackPower;
-				$('p[data-ind=' + myCharLoc + ']').text(characters[myCharLoc].healthPoints);
+				this.characters[myCharLoc].healthPoints -= this.characters[compCharLoc].counterAttackPower;
+				$('p[data-ind=' + myCharLoc + ']').text(this.characters[myCharLoc].healthPoints);
+
+				//update player 1's attackPower
+				this.characters[myCharLoc].healthPoints += myBaseAttack;
+
 
 				//update computer's health
-				characters[compCharLoc].healthPoints -= characters[myCharLoc].attackPower;
-				$('p[data-ind=' + compCharLoc + ']').text(characters[compCharLoc].healthPoints);
+				this.characters[compCharLoc].healthPoints -= this.characters[myCharLoc].attackPower;
+				$('p[data-ind=' + compCharLoc + ']').text(this.characters[compCharLoc].healthPoints);
+
+				//update computer's attackPower??
 
 				//print stats
-				$('p#gameStats').html("You attacked " + characters[compCharLoc].name + " for " + characters[myCharLoc].attackPower + " damage.<br />" + characters[compCharLoc].name + " attacked you back for " + characters[compCharLoc].counterAttackPower + " damage.");
+				$('p#gameStats').html("You attacked " + this.characters[compCharLoc].name + " for " + this.characters[myCharLoc].attackPower + " damage.<br />" + characters[compCharLoc].name + " attacked you back for " + this.characters[compCharLoc].counterAttackPower + " damage.");
 			}
 
 
@@ -60,7 +78,7 @@
 		var hpPara;
 		var charImg;
 
-		for(var i= 0; i < characters.length; i++){
+		for(var i= 0; i < game.characters.length; i++){
 
 			newDiv = $("<div></div>");
 			newDiv.addClass("imgCharacters");
@@ -70,11 +88,11 @@
 			newPara.attr({"id": "charName",
 						  "class": "text-center"});
 			newDiv.append(newPara);
-			newPara.text(characters[i].name);
+			newPara.text(game.characters[i].name);
 
 			charImg = $("<img>");
-			charImg.attr({'src': characters[i].src,
-						  'alt': characters[i].name});
+			charImg.attr({'src': game.characters[i].src,
+						  'alt': game.characters[i].name});
 			newDiv.append(charImg);
 
 
@@ -82,7 +100,7 @@
 			hpPara = $("<p></p>");
 			hpPara.addClass("hps text-center");
 			hpPara.attr('data-ind', i);
-			hpPara.text(characters[i].healthPoints);
+			hpPara.text(game.characters[i].healthPoints);
 			newDiv.append(hpPara);
 		}
 
